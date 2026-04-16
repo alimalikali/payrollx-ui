8ab13aba6b0ffb6f40f59e633c2d75bd831e8f40
Ali Zulfiqar <ali.polymath.dev@gmail.com>
2026-04-16 22:41:14 +0500

    refactor: remove unused slices and mock data from the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,18 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import employeesReducer from "./slices/employeesSlice";
-import attendanceReducer from "./slices/attendanceSlice";
-import leavesReducer from "./slices/leavesSlice";
-import payrollReducer from "./slices/payrollSlice";
-import aiInsightsReducer from "./slices/aiInsightsSlice";
 import uiReducer from "./slices/uiSlice";
 
 export const store = configureStore({
   reducer: {
-    employees: employeesReducer,
-    attendance: attendanceReducer,
-    leaves: leavesReducer,
-    payroll: payrollReducer,
-    aiInsights: aiInsightsReducer,
     ui: uiReducer,
   },
 });
